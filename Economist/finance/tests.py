f140b155e1e8ee52cd59ecb6bbd0eabda8b8cb6a
gipnocow <gipnocow@rambler.ru>
2016-12-15 00:58:07 +0300

    Добавила тесты, исправила статистику

--- Economist/finance/tests.py
+++ Economist/finance/tests.py
@@ -1,3 +1,4 @@
+import datetime
 from django.test import TestCase
 
 from .models import User, Contacts, Charge, Account
@@ -23,5 +24,27 @@ class AccountTest(TestCase):
         account = Account.create(100, 'test_account1', user)
         charge = Charge.create(-1000)
         account.add_charge(charge)
-        self.assertLess(account.total, 0)
+        self.assertEqual(100, account.total)
 
+    def test_last_transactions_statistic(self):
+        user = User.objects.create_user(username='user', password='user', email='user@test.ru')
+        account = Account.create(100, 'test_account_stat', user)
+
+        for i in range(1, 16):
+            charge = Charge.create(100+i, datetime.date(2016, 12, i))
+            account.add_charge(charge)
+        charge = Charge.create(-500, datetime.date.today())
+        account.add_charge(charge)
+        test_statistics = [{'income': 109, 'outcome': 0, 'date': datetime.date(2016, 12, 9)},
+                           {'income': 110, 'outcome': 0, 'date': datetime.date(2016, 12, 10)},
+                           {'income': 111, 'outcome': 0, 'date': datetime.date(2016, 12, 11)},
+                           {'income': 112, 'outcome': 0, 'date': datetime.date(2016, 12, 12)},
+                           {'income': 113, 'outcome': 0, 'date': datetime.date(2016, 12, 13)},
+                           {'income': 114, 'outcome': 0, 'date': datetime.date(2016, 12, 14)},
+                           {'income': 115, 'outcome': -500, 'date': datetime.date(2016, 12, 15)}]
+        test_statistics.reverse()
+        transactions_statistic = account.last_transactions_statistic()
+        for stat in transactions_statistic:
+            print(stat)
+        for i in range(len(transactions_statistic)):
+            self.assertEqual(transactions_statistic[i], test_statistics[i])
